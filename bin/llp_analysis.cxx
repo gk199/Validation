ab2f6f5d34a8e3794cf079f8d01970d5234bb377
gk199 <gillian.kopp@gmail.com>
2019-08-15 16:12:53 +0200

    Adding LLP analysis and histogram plotting code, updating BuildFile

---
+++ bin/llp_analysis.cxx
@@ -0,0 +1,342 @@
+// Script for LLP analysis
+#include "TMath.h"
+#include "TFile.h"
+#include "TTree.h"
+#include "TH1F.h"
+#include "TChain.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "L1Trigger/L1TNtuples/interface/L1AnalysisEventDataFormat.h"
+#include "L1Trigger/L1TNtuples/interface/L1AnalysisL1UpgradeDataFormat.h"
+#include "L1Trigger/L1TNtuples/interface/L1AnalysisRecoVertexDataFormat.h"
+#include "L1Trigger/L1TNtuples/interface/L1AnalysisCaloTPDataFormat.h"
+
+
+/* TODO: put errors in rates...
+creates the the rates and distributions for l1 trigger objects
+How to use:
+1. input the number of bunches in the run (~line 35)
+2. change the variables "newConditionsNtuples" and "oldConditionsNtuples" to ntuple paths
+3. If good run JSON is not applied during ntuple production, modify isGoodLumiSection()
+
+Optionally, if you want to rescale to a given instantaneous luminosity:
+1. input the instantaneous luminosity of the run (~line 32) [only if we scale to 2016 nominal]
+2. select whether you rescale to L=1.5e34 (~line606??...) generally have it setup to rescale
+nb: for 2&3 I have provided the info in runInfoForRates.txt
+*/
+
+// configurable parameters
+double numBunch = 1537; //the number of bunches colliding for the run of interest
+double runLum = 0.02; // 0.44: 275783  0.58:  276363 //luminosity of the run of interest (*10^34)
+double expectedLum = 1.15; //expected luminosity of 2016 runs (*10^34)
+
+void llp_analysis(bool newConditions, const std::string& inputFileDirectory);
+
+int main(int argc, char *argv[])
+{
+  bool newConditions = true;
+  std::string ntuplePath("");
+
+  if (argc != 3) {
+    std::cout << "Usage: llp_analysis.exe [new/def] [path to ntuples]\n"
+	      << "[new/def] indicates new or default (existing) conditions" << std::endl;
+    exit(1);
+  }
+  else {
+    std::string par1(argv[1]);
+    std::transform(par1.begin(), par1.end(), par1.begin(), ::tolower);
+    if(par1.compare("new") == 0) newConditions = true;
+    else if(par1.compare("def") == 0) newConditions = false;
+    else {
+      std::cout << "First parameter must be \"new\" or \"def\"" << std::endl;
+      exit(1);
+    }
+    ntuplePath = argv[2];
+  }
+
+  llp_analysis(newConditions, ntuplePath);
+
+  return 0;
+}
+
+// only need to edit this section if good run JSON
+// is not used during ntuple production
+bool isGoodLumiSection(int lumiBlock)
+{
+  if (lumiBlock >= 1
+      || lumiBlock <= 10000) {
+    return true;
+  }
+
+  return false;
+}
+
+void llp_analysis(bool newConditions, const std::string& inputFileDirectory){
+  
+  bool hwOn = false;   //are we using data from hardware? (upgrade trigger had to be running!!!)
+  bool emuOn = true;  //are we using data from emulator?
+
+  if (hwOn==false && emuOn==false){
+    std::cout << "exiting as neither hardware or emulator selected" << std::endl;
+    return;
+  }
+
+  std::string inputFile(inputFileDirectory);
+  inputFile += "/L1Ntuple_*.root";
+  std::string outputDirectory = "emu";  //***runNumber, triggerType, version, hw/emu/both***MAKE SURE IT EXISTS
+  std::string outputFilename = "llpanalysis_def.root";
+  if(newConditions) outputFilename = "llpanalysis_new_cond.root";
+  TFile* kk = TFile::Open( outputFilename.c_str() , "recreate");
+  // if (kk!=0){
+  //   cout << "TERMINATE: not going to overwrite file " << outputFilename << endl;
+  //   return;
+  // }
+
+
+  // make trees
+  std::cout << "Loading up the TChain..." << std::endl;
+  TChain * treeL1emu = new TChain("l1UpgradeEmuTree/L1UpgradeTree");
+  if (emuOn){
+    treeL1emu->Add(inputFile.c_str());
+  }
+  TChain * treeL1hw = new TChain("l1UpgradeTree/L1UpgradeTree");
+  if (hwOn){
+    treeL1hw->Add(inputFile.c_str());
+  }
+  TChain * eventTree = new TChain("l1EventTree/L1EventTree");
+  eventTree->Add(inputFile.c_str());
+
+  // In case you want to include PU info
+  // TChain * vtxTree = new TChain("l1RecoTree/RecoTree");
+  // if(binByPileUp){
+  //   vtxTree->Add(inputFile.c_str());
+  // }
+
+
+  TChain * treeL1TPemu = new TChain("l1CaloTowerEmuTree/L1CaloTowerTree");
+  if (emuOn){
+    treeL1TPemu->Add(inputFile.c_str());
+  }
+
+  TChain * treeL1TPhw = new TChain("l1CaloTowerTree/L1CaloTowerTree");
+  if (hwOn){
+    treeL1TPhw->Add(inputFile.c_str());
+  }
+
+  L1Analysis::L1AnalysisL1UpgradeDataFormat    *l1emu_ = new L1Analysis::L1AnalysisL1UpgradeDataFormat();
+  treeL1emu->SetBranchAddress("L1Upgrade", &l1emu_);
+  L1Analysis::L1AnalysisL1UpgradeDataFormat    *l1hw_ = new L1Analysis::L1AnalysisL1UpgradeDataFormat();
+  treeL1hw->SetBranchAddress("L1Upgrade", &l1hw_);
+  L1Analysis::L1AnalysisEventDataFormat    *event_ = new L1Analysis::L1AnalysisEventDataFormat();
+  eventTree->SetBranchAddress("Event", &event_);
+  // L1Analysis::L1AnalysisRecoVertexDataFormat    *vtx_ = new L1Analysis::L1AnalysisRecoVertexDataFormat();
+  // vtxTree->SetBranchAddress("Vertex", &vtx_);
+
+  L1Analysis::L1AnalysisCaloTPDataFormat    *l1TPemu_ = new L1Analysis::L1AnalysisCaloTPDataFormat();
+  treeL1TPemu->SetBranchAddress("CaloTP", &l1TPemu_);
+  L1Analysis::L1AnalysisCaloTPDataFormat    *l1TPhw_ = new L1Analysis::L1AnalysisCaloTPDataFormat();
+  treeL1TPhw->SetBranchAddress("CaloTP", &l1TPhw_);
+
+
+  // get number of entries
+  Long64_t nentries;
+  if (emuOn) nentries = treeL1emu->GetEntries();
+  else nentries = treeL1hw->GetEntries();
+  int goodLumiEventCount = 0;
+
+  std::string outputTxtFilename = "output_LLP/" + outputDirectory + "/extraInfo.txt";
+  std::ofstream myfile; // save info about the run
+  myfile.open(outputTxtFilename.c_str());
+  eventTree->GetEntry(0);
+  myfile << "run number = " << event_->run << std::endl;
+
+  // set parameters for histograms
+  // jet bins
+  int nJetBins = 400;
+  float jetLo = 0.;
+  float jetHi = 400.;
+  float jetBinWidth = (jetHi-jetLo)/nJetBins;
+
+
+  // tp bins
+  int nTpBins = 100;
+  float tpLo = 0.;
+  float tpHi = 100.;
+
+  std::string axR = ";Threshold E_{T} (GeV);rate (Hz)";
+  std::string axD = ";E_{T} (GeV);events/bin";
+
+  //make histos
+  TH1F* singleJetRates_emu = new TH1F("singleJetRates_emu", axR.c_str(), nJetBins, jetLo, jetHi);
+  TH1F* doubleJetRates_emu = new TH1F("doubleJetRates_emu", axR.c_str(), nJetBins, jetLo, jetHi);
+  TH1F* tripleJetRates_emu = new TH1F("tripleJetRates_emu", axR.c_str(), nJetBins, jetLo, jetHi);
+  TH1F* quadJetRates_emu = new TH1F("quadJetRates_emu", axR.c_str(), nJetBins, jetLo, jetHi);
+
+  TH1F* hcalTP_emu = new TH1F("hcalTP_emu", ";TP E_{T}; # Entries", nTpBins, tpLo, tpHi);
+  TH1F* ecalTP_emu = new TH1F("ecalTP_emu", ";TP E_{T}; # Entries", nTpBins, tpLo, tpHi);
+
+  TH1F* hcalTP_hw = new TH1F("hcalTP_hw", ";TP E_{T}; # Entries", nTpBins, tpLo, tpHi);
+  TH1F* ecalTP_hw = new TH1F("ecalTP_hw", ";TP E_{T}; # Entries", nTpBins, tpLo, tpHi);
+
+  /////////////////////////////////
+  // loop through all the entries//
+  /////////////////////////////////
+  for (Long64_t jentry=0; jentry<nentries; jentry++){
+    if((jentry%10000)==0) std::cout << "Done " << jentry  << " events of " << nentries << std::endl;
+
+
+    //lumi break clause
+    eventTree->GetEntry(jentry);
+    //skip the corresponding event
+    if (!isGoodLumiSection(event_->lumi)) continue;
+    goodLumiEventCount++;
+
+    //do routine for L1 emulator quantites
+    if (emuOn){
+
+      treeL1TPemu->GetEntry(jentry);
+      double tpEt(0.);
+      
+      for(int i=0; i < l1TPemu_->nHCALTP; i++){
+	tpEt = l1TPemu_->hcalTPet[i];
+	hcalTP_emu->Fill(tpEt);
+      }
+      for(int i=0; i < l1TPemu_->nECALTP; i++){
+	tpEt = l1TPemu_->ecalTPet[i];
+	ecalTP_emu->Fill(tpEt);
+      }
+
+      treeL1emu->GetEntry(jentry);
+      // get jetEt*, egEt*, tauEt, htSum, mhtSum, etSum, metSum
+      // ALL EMU OBJECTS HAVE BX=0...
+      double jetEt_1 = 0;
+      double jetEt_2 = 0;
+      double jetEt_3 = 0;
+      double jetEt_4 = 0;
+      if (l1emu_->nJets>0) jetEt_1 = l1emu_->jetEt[0];
+      if (l1emu_->nJets>1) jetEt_2 = l1emu_->jetEt[1];
+      if (l1emu_->nJets>2) jetEt_3 = l1emu_->jetEt[2];
+      if (l1emu_->nJets>3) jetEt_4 = l1emu_->jetEt[3];       
+
+
+      // for each bin fill according to whether our object has a larger corresponding energy
+      for(int bin=0; bin<nJetBins; bin++){
+        if( (jetEt_1) >= jetLo + (bin*jetBinWidth) ) singleJetRates_emu->Fill(jetLo+(bin*jetBinWidth));  //GeV
+      } 
+
+      for(int bin=0; bin<nJetBins; bin++){
+        if( (jetEt_2) >= jetLo + (bin*jetBinWidth) ) doubleJetRates_emu->Fill(jetLo+(bin*jetBinWidth));  //GeV
+      }  
+
+      for(int bin=0; bin<nJetBins; bin++){
+        if( (jetEt_3) >= jetLo + (bin*jetBinWidth) ) tripleJetRates_emu->Fill(jetLo+(bin*jetBinWidth));  //GeV
+      }  
+
+      for(int bin=0; bin<nJetBins; bin++){
+        if( (jetEt_4) >= jetLo + (bin*jetBinWidth) ) quadJetRates_emu->Fill(jetLo+(bin*jetBinWidth));  //GeV
+      }  
+
+
+    }// closes if 'emuOn' is true
+
+
+
+
+
+    //do routine for L1 hardware quantities
+    if (hwOn){
+
+      treeL1TPhw->GetEntry(jentry);
+      double tpEt(0.);
+      
+      for(int i=0; i < l1TPhw_->nHCALTP; i++){
+	tpEt = l1TPhw_->hcalTPet[i];
+	hcalTP_hw->Fill(tpEt);
+      }
+      for(int i=0; i < l1TPhw_->nECALTP; i++){
+	tpEt = l1TPhw_->ecalTPet[i];
+	ecalTP_hw->Fill(tpEt);
+      }
+
+
+      treeL1hw->GetEntry(jentry);
+      // get jetEt*, egEt*, tauEt, htSum, mhtSum, etSum, metSum
+      // ***INCLUDES NON_ZERO bx*** can't just read values off
+      double jetEt_1 = 0;
+      double jetEt_2 = 0;
+      double jetEt_3 = 0;
+      double jetEt_4 = 0;
+      for (UInt_t c=0; c<l1hw_->nJets; c++){
+        if (l1hw_->jetBx[c]==0 && l1hw_->jetEt[c] > jetEt_1){
+          jetEt_4 = jetEt_3;
+          jetEt_3 = jetEt_2;
+          jetEt_2 = jetEt_1;
+          jetEt_1 = l1hw_->jetEt[c];
+        }
+        else if (l1hw_->jetBx[c]==0 && l1hw_->jetEt[c] <= jetEt_1 && l1hw_->jetEt[c] > jetEt_2){
+          jetEt_4 = jetEt_3;
+          jetEt_3 = jetEt_2;      
+          jetEt_2 = l1hw_->jetEt[c];
+        }
+        else if (l1hw_->jetBx[c]==0 && l1hw_->jetEt[c] <= jetEt_2 && l1hw_->jetEt[c] > jetEt_3){
+          jetEt_4 = jetEt_3;     
+          jetEt_3 = l1hw_->jetEt[c];
+        }
+        else if (l1hw_->jetBx[c]==0 && l1hw_->jetEt[c] <= jetEt_3 && l1hw_->jetEt[c] > jetEt_4){   
+          jetEt_4 = l1hw_->jetEt[c];
+        }
+      }
+
+    }// closes if 'hwOn' is true
+
+  }// closes loop through events
+
+  //  TFile g( outputFilename.c_str() , "new");
+  kk->cd();
+  // normalisation factor for rate histograms (11kHz is the orbit frequency)
+  double norm = 11246*(numBunch/goodLumiEventCount); // no lumi rescale
+  //  double norm = 11246*(numBunch/goodLumiEventCount)*(expectedLum/runLum); //scale to nominal lumi
+
+  if (emuOn){
+    singleJetRates_emu->Scale(norm);
+    doubleJetRates_emu->Scale(norm);
+    tripleJetRates_emu->Scale(norm);
+    quadJetRates_emu->Scale(norm);
+
+    //set the errors for the rates
+    //want error -> error * sqrt(norm) ?
+
+    hcalTP_emu->Write();
+    ecalTP_emu->Write();
+    singleJetRates_emu->Write();
+    doubleJetRates_emu->Write();
+    tripleJetRates_emu->Write();
+    quadJetRates_emu->Write();
+
+  }
+
+  if (hwOn){
+
+    singleJetRates_hw->Scale(norm);
+    doubleJetRates_hw->Scale(norm);
+    tripleJetRates_hw->Scale(norm);
+    quadJetRates_hw->Scale(norm);
+
+
+    hcalTP_hw->Write();
+    ecalTP_hw->Write();
+    singleJetRates_hw->Write();
+    doubleJetRates_hw->Write();
+    tripleJetRates_hw->Write();
+    quadJetRates_hw->Write();
+
+  }
+  myfile << "using the following ntuple: " << inputFile << std::endl;
+  myfile << "number of colliding bunches = " << numBunch << std::endl;
+  myfile << "run luminosity = " << runLum << std::endl;
+  myfile << "expected luminosity = " << expectedLum << std::endl;
+  myfile << "norm factor used = " << norm << std::endl;
+  myfile << "number of good events = " << goodLumiEventCount << std::endl;
+  myfile.close(); 
+}//closes the function 'rates'
