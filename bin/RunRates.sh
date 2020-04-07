33d19d938a4cc3719792c6e47cde6bb932dc010a
gk199 <gillian.kopp@gmail.com>
2020-04-07 23:55:38 +0200

    Updating with only rates.cxx for each

--- bin/RunRates.sh
+++ bin/RunRates.sh
@@ -2,38 +2,8 @@ echo "Compiling rates.cxx, rates_TPin4groups.cxx, and draw_rates.cxx files"
 echo " "
 scram b -j 8
 echo " "
-echo "Starting with running rates_TPin4groups.cxx where HCAL TPs are divided into four groups based on closest L1 jet, and then restrict with DR cone"
-echo "LLP mh=1000 GeV, ctau=10 m"
-rates_TPin4groups.exe new ../mh1000_pl10000/
-mv rates_new_cond.root rates_new_cond_pl10000_4L1Jets.root
 echo " "
-echo "LLP mh=1000 GeV, ctau=1 m"
-rates_TPin4groups.exe new ../mh1000_pl1000/
-mv rates_new_cond.root rates_new_cond_pl1000_4L1Jets.root
-echo " "
-echo "LLP mh=1000 GeV, ctau=0.5 m"
-rates_TPin4groups.exe new ../mh1000_pl500/
-mv rates_new_cond.root rates_new_cond_pl500_4L1Jets.root
-echo " "
-echo "LLP mh=500 GeV, ctau=0.5 m, no PU"
-rates_TPin4groups.exe new ../mh1000_pl500_noPU/
-mv rates_new_cond.root rates_new_cond_pl500_noPU_4L1Jets.root
-echo " "
-echo "QCD"
-rates_TPin4groups.exe new ../QCD/
-mv rates_new_cond.root rates_new_cond_QCD_4L1Jets.root
-echo " "
-echo "Neutrino gun new conditions"
-rates_TPin4groups.exe new ../NeutrinoGun/
-mv rates_new_cond.root rates_new_cond_nugunTDC_4L1Jets.root
-echo " "
-echo "Making overlay plots from draw_rates.exe"
-draw_rates.exe
-echo "Moving output plots to a new directory"
-mv plots/* plots_TPin4groups/
-echo " "
-echo " "
-echo "Starting with running rates.cxx where all HCAL TPs in a DR cone of L1 jet are considered"
+echo "Starting with running rates.cxx where all HCAL TPs in a DR cone of L1 jet are considered OR HCAL TP double counting is prevented -- change in code!"
 echo "LLP mh=1000 GeV, ctau=10 m"
 rates.exe new ../mh1000_pl10000/
 mv rates_new_cond.root rates_new_cond_pl10000_4L1Jets.root
@@ -46,7 +16,7 @@ echo "LLP mh=1000 GeV, ctau=0.5 m"
 rates.exe new ../mh1000_pl500/
 mv rates_new_cond.root rates_new_cond_pl500_4L1Jets.root
 echo " "
-echo "LLP mh=500 GeV, ctau=0.5 m, no PU"
+echo "LLP mh=1000 GeV, ctau=0.5 m, no PU"
 rates.exe new ../mh1000_pl500_noPU/
 mv rates_new_cond.root rates_new_cond_pl500_noPU_4L1Jets.root
 echo " "
