3efa63b453465dac5a27d919a92c6a48b5eddf55
gk199 <gillian.kopp@gmail.com>
2020-04-11 00:16:12 +0200

    Running over private production mh=350 GeV files

--- bin/RunRates.sh
+++ bin/RunRates.sh
@@ -4,10 +4,6 @@ scram b -j 8
 echo " "
 echo " "
 echo "Starting with running rates.cxx where all HCAL TPs in a DR cone of L1 jet are considered OR HCAL TP double counting is prevented -- change in code!"
-echo "LLP mh=1000 GeV, ctau=10 m"
-rates.exe new ../mh1000_pl10000/
-mv rates_new_cond.root rates_new_cond_pl10000_4L1Jets.root
-echo " "
 echo "LLP mh=1000 GeV, ctau=1 m"
 rates.exe new ../mh1000_pl1000/
 mv rates_new_cond.root rates_new_cond_pl1000_4L1Jets.root
@@ -31,4 +27,18 @@ echo " "
 echo "Making overlay plots from draw_rates.exe"
 draw_rates.exe
 echo "Moving output plots to a new directory"
-mv plots/* plots_DRaroundTPandL1/
+mv plots/* plots_DRaroundTPandL1_mh1000/
+echo " "
+echo " "
+echo "LLP mh=350 GeV, ctau=1 m"
+rates.exe new ../mh350_pl160_pl1000/
+mv rates_new_cond.root rates_new_cond_pl1000_4L1Jets.root
+echo " "
+echo "LLP mh=350 GeV, ctau=0.5 m"
+rates.exe new ../mh350_pl160_pl500/
+mv rates_new_cond.root rates_new_cond_pl500_4L1Jets.root
+echo " "
+echo "Making overlay plots from draw_rates.exe"
+draw_rates.exe
+echo "Moving output plots to a new directory"
+mv plots/* plots_DRaroundTPandL1_mh350/
