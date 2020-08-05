c552a85db9ee34eb2734b9d85b9c8204eedb6705
gk199 <gillian.kopp@gmail.com>
2020-08-06 01:20:47 +0200

    saving L1 jet ET distributions for 110X 106X comparison

--- RunRates_noDepth0.sh
+++ RunRates_noDepth0.sh
@@ -8,12 +8,13 @@ rates.exe new /eos/cms/store/group/dpg_hcal/comm_hcal/gillian/LLP_Run3/TimingTri
 mv rates_new_cond.root rates_new_cond_QCD.root
 echo " "
 echo "Neutrino gun new and old conditions"
-rates.exe new /eos/cms/store/group/dpg_hcal/comm_hcal/gillian/LLP_Run3/TimingTrigger/Neutrino_Pt-2to20_gun_HCAL/CRAB3_NuGun_TDC_MC/200722_142743/0000/
-rates_original.exe def /eos/cms/store/group/dpg_hcal/comm_hcal/gillian/LLP_Run3/TimingTrigger/Neutrino_Pt-2to20_gun_HCAL/CRAB3_NuGun_TDC_MC/200722_142743/0000/
+rates.exe new /eos/cms/store/group/dpg_hcal/comm_hcal/gillian/LLP_Run3/TimingTrigger/Neutrino_Pt-2to20_gun_HCAL/CRAB3_NuGun_TDC_MC/200722_142743/0000
+rates_original.exe def /eos/cms/store/group/dpg_hcal/comm_hcal/gillian/LLP_Run3/TimingTrigger/Neutrino_Pt-2to20_gun_HCAL/CRAB3_NuGun_TDC_MC/200722_142743/0000
 echo " "
 echo "Rates plots from draw_rates.exe"
 draw_rates.exe
 mv plots/*Rates_emu.pdf /eos/user/g/gkopp/www/HCAL_LLP/TimingBit/OfficialProduction/ADC50_3ns_no1HE4HB/
+mv rates_new_cond.root rates_new_cond_110X_nugun.root
 echo " "
 echo "LLP mh=125 GeV, ctau=3 m"
 rates.exe new /eos/cms/store/group/dpg_hcal/comm_hcal/gillian/LLP_Run3/TimingTrigger/HTo2LongLivedTo4b_MH-125_MFF-50_CTau-3000mm_TuneCP5_14TeV_pythia8_HCAL/CRAB3_mh125_ctau3000_TDC_MC/200722_185526/0000
@@ -25,6 +26,8 @@ mv rates_new_cond.root rates_new_cond_LLP_pl30000.root
 echo " "
 echo "Making overlay plots from draw_timingbit.exe"
 draw_timingbit_mh125.exe
+mv rates_new_cond_LLP_pl30000.root rates_new_cond_LLP_mh125_pl30000.root
+mv rates_new_cond_LLP_pl3000.root rates_new_cond_LLP_mh125_pl3000.root
 mv plots/ADC50_3ns_4JetMultHBOverlay.pdf /eos/user/g/gkopp/www/HCAL_LLP/TimingBit/OfficialProduction/ADC50_3ns_no1HE4HB/ADC50_3ns_no1HE4HB_4JetMultHBOverlay_mh125.pdf
 mv plots/ADC50_3ns_4JetMultHEOverlay.pdf /eos/user/g/gkopp/www/HCAL_LLP/TimingBit/OfficialProduction/ADC50_3ns_no1HE4HB/ADC50_3ns_no1HE4HB_4JetMultHEOverlay_mh125.pdf
 mv plots/ADC50_3ns_4JetMultHBHEOverlay.pdf /eos/user/g/gkopp/www/HCAL_LLP/TimingBit/OfficialProduction/ADC50_3ns_no1HE4HB/ADC50_3ns_no1HE4HB_4JetMultHBHEOverlay_mh125.pdf
@@ -43,6 +46,9 @@ mv rates_new_cond.root rates_new_cond_LLP_pl10000.root
 echo " "
 echo "Making overlay plots from draw_timingbit.exe"
 draw_timingbit_mh250.exe
+mv rates_new_cond_LLP_pl500.root rates_new_cond_LLP_mh250_pl500.root
+mv rates_new_cond_LLP_pl1000.root rates_new_cond_LLP_mh250_pl1000.root
+mv rates_new_cond_LLP_pl10000.root rates_new_cond_LLP_mh250_pl10000.root
 mv plots/ADC50_3ns_4JetMultHBOverlay.pdf /eos/user/g/gkopp/www/HCAL_LLP/TimingBit/OfficialProduction/ADC50_3ns_no1HE4HB/ADC50_3ns_no1HE4HB_4JetMultHBOverlay_mh250.pdf
 mv plots/ADC50_3ns_4JetMultHEOverlay.pdf /eos/user/g/gkopp/www/HCAL_LLP/TimingBit/OfficialProduction/ADC50_3ns_no1HE4HB/ADC50_3ns_no1HE4HB_4JetMultHEOverlay_mh250.pdf
 mv plots/ADC50_3ns_4JetMultHBHEOverlay.pdf /eos/user/g/gkopp/www/HCAL_LLP/TimingBit/OfficialProduction/ADC50_3ns_no1HE4HB/ADC50_3ns_no1HE4HB_4JetMultHBHEOverlay_mh250.pdf
@@ -62,6 +68,9 @@ mv rates_new_cond.root rates_new_cond_LLP_pl10000.root
 echo " "
 echo "Making overlay plots from draw_timingbit.exe"
 draw_timingbit_mh350.exe
+mv rates_new_cond_LLP_pl500.root rates_new_cond_LLP_mh350_pl500.root
+mv rates_new_cond_LLP_pl1000.root rates_new_cond_LLP_mh350_pl1000.root
+mv rates_new_cond_LLP_pl10000.root rates_new_cond_LLP_mh350_pl10000.root
 mv plots/ADC50_3ns_4JetMultHBOverlay.pdf /eos/user/g/gkopp/www/HCAL_LLP/TimingBit/OfficialProduction/ADC50_3ns_no1HE4HB/ADC50_3ns_no1HE4HB_4JetMultHBOverlay_mh350.pdf
 mv plots/ADC50_3ns_4JetMultHEOverlay.pdf /eos/user/g/gkopp/www/HCAL_LLP/TimingBit/OfficialProduction/ADC50_3ns_no1HE4HB/ADC50_3ns_no1HE4HB_4JetMultHEOverlay_mh350.pdf
 mv plots/ADC50_3ns_4JetMultHBHEOverlay.pdf /eos/user/g/gkopp/www/HCAL_LLP/TimingBit/OfficialProduction/ADC50_3ns_no1HE4HB/ADC50_3ns_no1HE4HB_4JetMultHBHEOverlay_mh350.pdf
@@ -76,6 +85,8 @@ mv rates_new_cond.root rates_new_cond_LLP_pl100000.root
 echo " "
 echo "Making overlay plots from draw_timingbit.exe"
 draw_timingbit_mh1000.exe
+mv rates_new_cond_LLP_pl10000.root rates_new_cond_LLP_mh1000_pl10000.root
+mv rates_new_cond_LLP_pl100000.root rates_new_cond_LLP_mh1000_pl100000.root
 mv plots/ADC50_3ns_4JetMultHBOverlay.pdf /eos/user/g/gkopp/www/HCAL_LLP/TimingBit/OfficialProduction/ADC50_3ns_no1HE4HB/ADC50_3ns_no1HE4HB_4JetMultHBOverlay_mh1000.pdf
 mv plots/ADC50_3ns_4JetMultHEOverlay.pdf /eos/user/g/gkopp/www/HCAL_LLP/TimingBit/OfficialProduction/ADC50_3ns_no1HE4HB/ADC50_3ns_no1HE4HB_4JetMultHEOverlay_mh1000.pdf
 mv plots/ADC50_3ns_4JetMultHBHEOverlay.pdf /eos/user/g/gkopp/www/HCAL_LLP/TimingBit/OfficialProduction/ADC50_3ns_no1HE4HB/ADC50_3ns_no1HE4HB_4JetMultHBHEOverlay_mh1000.pdf
