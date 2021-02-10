1e9a69b682ef0d723659e00a6d86c1050d9b65a8
gk199 <gillian.kopp@gmail.com>
2021-02-11 00:30:07 +0100

    L1 plots requested, start on efficiency plot

---
+++ L1plots_eff_rates/efficiency.C
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <fstream>
+#include <new>
+#include<climits>
+#include "TFile.h"
+#include "TTree.h"
+#include "TH1.h"
+#include "TPad.h"
+#include "TCanvas.h"
+#include "TH1F.h"
+#include "TGraphErrors.h"
+#include "TEfficiency.h"
+#include "TF1.h"
+#include "TH2.h"
+#include "TProfile.h"
+
+#include "TStyle.h"
+#include "TAttFill.h"
+#include "TPaveStats.h"
+#include "TMinuit.h"
+#include "TPostScript.h"
+#include "TFitResult.h"
+#include "TFitResultPtr.h"
+#include "TRandom.h"
+#include "TPaletteAxis.h"
+#include "TLegend.h"
+#include "TLatex.h"
+#include "TMath.h"
+#include "TCanvas.h"
+
+using namespace std;
+
+Double_t EMG(Double_t *x, Double_t *par){
+  double x_prime = par[1]*(x[0] - par[0]);
+  double term_1 = 0.5 * (1 + TMath::Erf(x_prime));
+  double exp_modif = TMath::Exp(-par[2]*(x_prime - par[2]*0.5) );
+  double term_2 = 0.5 * (1 + TMath::Erf(x_prime - par[2]));
+  double func = term_1 - exp_modif*term_2;
+  return func;
+}
+
+void efficiency(){
+  TCanvas *c1 =new TCanvas("c1", " ", 0, 0,700,1000);
+  c1->Range(0,0,1,1);
+  c1->SetFillColor(0);
+  c1->SetBorderMode(0);
+  c1->SetBorderSize(2);
+  c1->SetFrameBorderMode(0);
+  c1->Draw();
+  c1->SetGrid();
+  gStyle->SetOptStat(0);
+
+  TFile *g1 =TFile::Open("../rates_new_cond_LLP_mh125_pl3000.root");
+  TH1F *h1 = (TH1F*)g1->Get("HTdistribution_emu");
+  TH1F *h2 = (TH1F*)g1->Get("HTdistribution_trig_emu");
+
+  h1->SetLineColorAlpha(kWhite, 1.);
+  h1->SetTitle("");
+  h1->GetXaxis()->SetTitle("H_{T} [GeV]");
+  h1->GetXaxis()->SetTitleSize(0.045);
+  h1->GetXaxis()->SetTitleOffset(1.1);
+  h1->GetYaxis()->SetTitle("Efficiency");
+  h1->GetYaxis()->SetTitleSize(0.045);
+  h1->GetYaxis()->SetTitleOffset(1.1);
+  h1->GetYaxis()->SetRangeUser(0.,1.5);
+  h1->Draw();
+
+  c1->Update();
+  c1->RedrawAxis();
+
+  TEfficiency* pEff1 = 0;
+
+  if(TEfficiency::CheckConsistency(*h2,*h1))
+    {
+      pEff1 = new TEfficiency(*h2,*h1);
+      pEff1->SetLineWidth(3.);
+      pEff1->SetLineColor(kBlack);
+      pEff1->Draw("same");
+    }
+
+  TLegend *legend1 = new TLegend(0.15, 0.7, 0.6, 0.8);
+  legend1->SetTextFont(42);
+  legend1->SetLineColor(0);
+  legend1->SetTextSize(0.04);
+  legend1->SetFillColor(0);
+  legend1->AddEntry(pEff1, "One delayed L1 jet", "l");
+
+  legend1->Draw("same");
+}
+
